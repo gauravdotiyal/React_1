ec5e36a8761ef33f47ee73fca2945c53ffbb7e52
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-03-08 13:57:00 +0530

    git commit -m "setevents for deleting"

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { useState} from 'react';
  
  
 function App() {
-  const [name,setName]=useState('mario')
+ 
   const [events,setEvents]=useState([
     {title:"mario birthday bash",id:1},
     {title:"browser's live stream",id:2},
     {title:"race on moo moo farm", id:3}
   ])
-  const handleclick =()=>{
-    setName('luigi')
-    console.log(name);
+  const handleclick =(id)=>{
+     setEvents(events.filter((event)=>{
+       return  id!==event.id;
+     }))
+     console.log(id);
   }
   return (
-    <div className='App'> 
-      <h1> My name is {name} </h1>
-    <button onClick={handleclick}>Change Name</button>
+    <div className='App'>  
       {events.map((event,index)=> (
          <div key={event.id}>
          <h2>{index} - {event.title}</h2>
+         <button onClick={()=> handleclick(event.id)}>Delete events</button>
        </div>
       ))}
     </div> 
