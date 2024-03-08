82b533756de69d087f487ff7a1b901d34d336704
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-03-08 13:03:47 +0530

    chapter 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css';   
+import { useState} from 'react';
+ 
+ 
 function App() {
+  const [name,setName]=useState('mario')
+  const [events,setEvents]=useState([
+    {title:"mario birthday bash",id:1},
+    {title:"browser's live stream",id:2},
+    {title:"race on moo moo farm", id:3}
+  ])
+  const handleclick =()=>{
+    setName('luigi')
+    console.log(name);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'> 
+      <h1> My name is {name} </h1>
+    <button onClick={handleclick}>Change Name</button>
+      {events.map((event,index)=> (
+         <div key={event.id}>
+         <h2>{index} - {event.title}</h2>
+       </div>
+      ))}
+    </div> 
   );
 }
 
 export default App;
+
+
