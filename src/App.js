049f8411e89f95fa9680db7ad8aa287e57062a77
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-03-08 15:13:46 +0530

    showevents function

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import { useState} from 'react';
  
  
 function App() {
- 
-  const [events,setEvents]=useState([
+    const [showEvents,setshowevents]=useState(true)
+    const [events,setEvents]=useState([
     {title:"mario birthday bash",id:1},
     {title:"browser's live stream",id:2},
-    {title:"race on moo moo farm", id:3}
+    {title:"race on moo moo farm", id:3} 
   ])
+   
+  // console.log(showEvents)
+
   const handleclick =(id)=>{
-     setEvents(events.filter((event)=>{
-       return  id!==event.id;
-     }))
+     setEvents((prevEvents)=>{
+      return prevEvents.filter((event)=>{
+      return id!==event.id;
+      })
+     })
      console.log(id);
   }
   return (
     <div className='App'>  
-      {events.map((event,index)=> (
+      {showEvents && (<div>
+      <button onClick={()=>setshowevents(false)}>Hide events</button>
+      </div>)}
+
+      {!showEvents && (<div>
+      <button onClick={()=>setshowevents(true)}>Show events</button>
+      </div>)}
+
+      {showEvents && events.map((event,index)=> (
          <div key={event.id}>
          <h2>{index} - {event.title}</h2>
          <button onClick={()=> handleclick(event.id)}>Delete events</button>
@@ -30,3 +43,4 @@ function App() {
 export default App;
 
 
+// https://github.com/gauravdotiyal/React_Learning
